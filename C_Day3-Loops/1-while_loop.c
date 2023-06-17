2177030de8a7a8d61eb2afb87e96f592b48d4f47
Sams-Ddev <sams.alxstudent@gmail.com>
2023-06-17 04:15:23 -0700

    while loops example 1

---
+++ C_Day3-Loops/1-while_loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	/*Definition of local variable*/
+	int v_a = 10;
+
+	/*while loop execution*/
+	while(v_a < 20){
+		printf("Value of a: %d\n", v_a);
+		v_a++;
+	}
+	return(0);
+}
