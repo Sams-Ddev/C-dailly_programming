d132f6defacc6a7e521ebb930c558c6fc36479de
Sams-Ddev <sams.alxstudent@gmail.com>
2023-06-17 08:50:17 -0700

    Betty-style formating

--- C_Day3-Loops/1-while_loop.c
+++ C_Day3-Loops/1-while_loop.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 
-int main(){
+/*
+*description of main the main function is a while loop
+*that prints the next 10 interger counts starting from 10
+*
+**/
+
+int main(void)
+{
 	/*Definition of local variable*/
 	int v_a = 10;
 
 	/*while loop execution*/
-	while(v_a < 20){
+	while (v_a < 20)
+	{
 		printf("Value of a: %d\n", v_a);
 		v_a++;
 	}
-	return(0);
+	return (0);
 }
