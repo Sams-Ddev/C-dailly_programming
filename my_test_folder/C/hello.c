54d31c6459b0c204a772ff16ee0a8464fea3dba2
Sams-Ddev <sams.alxstudent@gmail.com>
2023-06-17 01:48:19 -0700

    Day 1 Hello world basic

---
+++ my_test_folder/C/hello.c
@@ -0,0 +1,14 @@
+/**
+ *Program: The rudimentary hello world program 
+ * 
+ *Funtions prints "hello world to the standard output"
+ *
+ * */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello, World!\n");
+	return(0);
+}
---
+++ my_test_folder/C/if..else.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * This small program demonstrate the  use of the following concepts
+ *
+ * 1.printf functions
+ * 2.scanf()-a funtion that takes user input
+ * 3.If..else statements
+ * 4.%d is a format specifier used with the printf and scanf
+ *
+ * I hope you like it
+ *
+ * */
+int main(void)
+{
+	int age;
+	scanf("What is your age:");
+}
