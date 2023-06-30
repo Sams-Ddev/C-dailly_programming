6d6daabf0e787fb508fb17bfe2450ce92ce92800
sams-ddev <sams.alxstudent@gmail.com>
2023-06-30 10:37:36 +0000

    rework on betty styling

--- C_Day2-Functions/1-Func.c
+++ C_Day2-Functions/1-Func.c
@@ -1,27 +1,42 @@
-#include <stdio.h>	/*This is a header file*/
+#include <stdio.h>
+/*This is a header file provides input/output functionalities*/
 
 /**
  * In the below code, we have created a function named 'func_square'
- * that takes an integer parameter 'num'. 
- * The function calculates the square of 'num'
+ * @num : takes an integer parameter 'num'.
+ * func_square - function calculates the square of 'num'.
  *
  * Returns - the result of the execution.
+ *
+ * main - start of prigram
+ * return - 0 (for success)
  */
 
-/*We define the function (func_square) below*/
+/*We declare the function (func_square) below*/
+int func_square(int num);
+
+/*Definition of the function (func_square)*/
 int func_square(int num)
 {
 	int num;
 	int result;
 
-	result = num*num;
+	result = num * num;
 
 	return (result);
 }
 
-/**/
+/*main - start of program*/
 int main(void)
 {
-	func_square(int num);
-	return (num*num);
+	int num1;
+
+	printf("Enter a number:");	/*takes user input*/
+	scanf("%d", &num1);	/*stores user input in var num1*/
+
+	int result1 = func_square(num1);	/*calling the function*/
+
+	printf("The square of %d is %d", num1, result1);
+
+	return (0);
 }
