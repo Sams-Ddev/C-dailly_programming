8b5a7bac9c78a47a83670ff1dacdbb982ff7df0f
sams-ddev <sams.alxstudent@gmail.com>
2023-06-30 09:56:27 +0000

    function (func_square construction)

---
+++ C_Day2-Functions/1-Func.c
@@ -0,0 +1,27 @@
+#include <stdio.h>	/*This is a header file*/
+
+/**
+ * In the below code, we have created a function named 'func_square'
+ * that takes an integer parameter 'num'. 
+ * The function calculates the square of 'num'
+ *
+ * Returns - the result of the execution.
+ */
+
+/*We define the function (func_square) below*/
+int func_square(int num)
+{
+	int num;
+	int result;
+
+	result = num*num;
+
+	return (result);
+}
+
+/**/
+int main(void)
+{
+	func_square(int num);
+	return (num*num);
+}
