89987fb1bf1b15b7299e761fb841c1988935c6eb
sams-ddev <sams.alxstudent@gmail.com>
2023-06-30 13:36:25 +0000

    creating findMax function

---
+++ C_Day2-Functions/3-findMax.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/*We include the <stdio.h> header file for input/output operations.*/
+
+/**
+ * main - start of the program
+ * @findMax - takes three integer parameters a, b, c
+ * and compares the values of the giver int parameters
+ * @findMax Return - maximun value
+ *
+ * Return - 0 (for execution success)
+ */
+
+int findMax(int a, int b, int c);
+
+int main(void)
+{
+	printf("");
+}
+
+/*Declaring function findMax*/
+int findMax(int a, int b, int c)
+{
+	a
+}
