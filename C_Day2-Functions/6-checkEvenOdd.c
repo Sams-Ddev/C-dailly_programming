951749bf2e7498575e07985ab230e07862a0fcc5
sams-ddev <sams.alxstudent@gmail.com>
2023-07-01 17:18:35 +0000

    6-checkEvenOdd.c

---
+++ C_Day2-Functions/6-checkEvenOdd.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * checkEvenOdd - takes an interger parameter 'num'
+ * and checks if its an even number or odd number
+ * The function prints the corresponding mesage
+ *
+ * main - calls the 'checkEvenOdd' function with a number
+ */
+
+//Function declaration
+int checkEvenOdd(int num);
+
+//Function definition
+int checkEvenOdd(int num)
+{
+	if 
+}
+
