d9361995a6b5257f29ea5535228023e5447415e0
sams-ddev <sams.alxstudent@gmail.com>
2023-07-04 11:21:14 +0000

    c basic files

---
+++ C_practice/1-c_hello-world/0x01-cBasic.c
@@ -0,0 +1,23 @@
+/**
+ * A C program basically consists of the following parts −
+ *
+ * - Preprocessor Commands
+ * - Functions
+ * - Variables
+ * - Statements & Expressions
+ * - Comments
+ *   
+ * Let us look at a simple code that would print the words "Hello World" −
+ */
+
+
+#include <stdio.h>	//preprocessor command
+
+//this is the function
+int main() {
+
+   /* this is the expression/statement in C */
+   printf("Hello, World! \n");
+
+   return 0;
+}
