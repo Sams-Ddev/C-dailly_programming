f6740c3924671921b6da70d7299cb88089582fd9
sams-ddev <sams.alxstudent@gmail.com>
2023-07-13 09:06:34 +0000

    a function that prints a array of strings

---
+++ C_programming-concepts/malloc/print_sch.c
@@ -0,0 +1,25 @@
+/**
+ * print_school - prints "ALx Student"
+ *
+ * Return - nothing.
+ */
+
+void print_school(void)
+{
+	char str[] = "ALx Student";
+
+	str[0] = 'a';
+	printf("%s\n", str);
+}
+
+/**
+ * main - concept introduction
+ *
+ * Return - 0
+ */
+
+int main(void)
+{
+	print_school();
+	return (0);
+}
