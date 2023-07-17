4eeda6550dabd7b16e59dc09b316eba5d6f5a6cc
sams-ddev <sams.alxstudent@gmail.com>
2023-07-17 11:55:19 +0000

    recompiled

--- C_programming-concepts/macros/0x01-macro1.c
+++ C_programming-concepts/macros/0x01-macro1.c
@@ -11,7 +11,7 @@
 
 int main(void)
 {
-	PRINT_ARGUMENT("Hello! world");	// expands to: printf("Argument: %s\n", "Hello")
+	PRINT_ARGUMENT(Hello! world);	// expands to: printf("Argument: %s\n", "Hello")
 	return (0);
 }
 
