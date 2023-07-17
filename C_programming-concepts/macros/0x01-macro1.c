6fcfee9144dc37ffd8a19ff5e0d21ea20f856490
sams-ddev <sams.alxstudent@gmail.com>
2023-07-17 11:50:58 +0000

    wrote a 'hello! using macros'

---
+++ C_programming-concepts/macros/0x01-macro1.c
@@ -0,0 +1,17 @@
+/**
+ * Macro Argument Syntax:
+ * The syntax for defining a macro with arguments is as follows:
+ *
+ * Copy code
+ * #define MACRO_NAME(parameter1, parameter2, ...) code_snippet
+ */
+
+#include <stdio.h>
+#define PRINT_ARGUMENT(x) printf("Argument to print : %s\n", #x)
+
+int main(void)
+{
+	PRINT_ARGUMENT("Hello! world");
+	return (0);
+}
+
