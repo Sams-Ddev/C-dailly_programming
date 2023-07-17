8de5f238c4900ebdf3afa56e12fa6a477ef801eb
sams-ddev <sams.alxstudent@gmail.com>
2023-07-17 13:15:51 +0000

    concatenation of macros

---
+++ C_programming-concepts/macros/0x03-mConcatenation.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * Concatenation:
+ * The ## operator can concatenate macro arguments or other tokens.
+ * Example below:
+ */
+
+#define CONCATENATION(x, y) x##y
+
+int main(void)
+{
+	printf("This simple program uses MACROS to concatenate two numbers💻🐱‍👤");
+	prinft("Enter first number: ");
+	scanf("%d",);
+}
