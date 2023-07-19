e20f005e25e5d8b67d8466f469aec179b8966746
sams-ddev <sams.alxstudent@gmail.com>
2023-07-19 10:13:45 +0000

    accessing struct elements with the '.' symbol

--- C_programming-concepts/structs/0x01-user1.c
+++ C_programming-concepts/structs/0x01-user1.c
@@ -17,5 +17,9 @@ int main(void)
 {
 	struct User user;
 
+	user.name = "Fooli Bar";	/*accessing the elements of the struct by using '.' symbo;*/
+	user.email = "sams.ccoder@int.com";
+	user.age = 98;
+
 	return (0);
 }
