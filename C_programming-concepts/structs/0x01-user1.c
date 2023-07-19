b55cd7ceaebcebe7a5ae9d77242d06499d325f04
sams-ddev <sams.alxstudent@gmail.com>
2023-07-19 10:05:39 +0000

    C concepts; structures

---
+++ C_programming-concepts/structs/0x01-user1.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+/**
+ * structure declaration - you can define a structure in the global
+ * scope of your program (outside of all your functions, just like
+ * the functions prototypes)
+ */
+
+struct User
+{
+	char *name;
+	char *email;
+	int age;
+};
+
+int main(void)
