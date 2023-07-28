2e2b275401ab722fdb4611a128d4c1ff3c855483
sams-ddev <sams.alxstudent@gmail.com>
2023-07-28 19:52:49 +0000

    rework on ‘int (*)(void)’ makes integer from pointer w

---
+++ C_practice/get_putchar.c/charCount1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ * charCount - the second version with minimun code write ups
+ * optimised and simple 
+ *
+ * have a look
+ */
+
+int main(char c)
+{
+	int c;
+
+	while ((c = getchar) != EOF)
+		putchar(c);
+}
