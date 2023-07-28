32ce8f1ae095fbaccebf2ddb79b0be581ee2ef0f
sams-ddev <sams.alxstudent@gmail.com>
2023-07-28 19:39:01 +0000

    wrote charCount.c version one

---
+++ C_practice/get_putchar.c/charCount.c
@@ -0,0 +1,18 @@
+/* File copying
+ * using the inbuilt 'getchar() and putchar()'
+ *
+ * The small program copy char from input and 
+ * prints it into the the standard output
+*/
+
+/* First version of code*/
+int main()
+{
+	int c;
+	c = getchar();
+	while (c != EOF)
+	{
+		putchar(c);
+		c = getchar();
+	}
+}
