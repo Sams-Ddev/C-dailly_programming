e5d8255318c8edd5c8ce244195208ae2eaa492f6
sams-ddev <sams.alxstudent@gmail.com>
2023-07-29 14:59:56 +0000

    wrote a programme that counts new tab, woeds, blanks

--- C_practice/get_putchar.c/wordCount.c
+++ C_practice/get_putchar.c/wordCount.c
@@ -10,9 +10,30 @@
 
 #include <stdio.h>
 
+#define IN 1	/* Inside a word*/
+#define OUT 0	/* outside a word*/
+
 /* count lines, words, and characters in input */
 
 int main()
 {
-	int 
+	int c, nl, nw, nc, state;
+	/*c - char, nl- newline, nw - new word, nc- new char, state- inside a word or not*/
+
+	state = OUT;
+	nl = nw = nc = 0;
+	while ((c = getchar()) != EOF)
+	{
+		++nc;
+		if (c == '\n')
+			++nl;
+		if (c == ' ' || c == '\n' || c == '\t')
+			state = OUT;
+		else if (state == OUT){
+			state = IN;
+			++nw;
+		}
+
+	}
+	print("%d %d %d\n", nl, nw, nc);
 }
