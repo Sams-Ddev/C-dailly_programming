747b3dc9d5c2c6886c725535fd38068c8c8c04af
sams-ddev <sams.alxstudent@gmail.com>
2023-07-29 13:44:33 +0000

    wrote a word count

--- C_practice/get_putchar.c/charCount.c
+++ C_practice/get_putchar.c/charCount.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /* File copying
  * using the inbuilt 'getchar() and putchar()'
  *
---
+++ C_practice/get_putchar.c/wordCount.c
@@ -0,0 +1,18 @@
+/*
+ * A function - 
+ * wordCount() - a function that counts word, lines, tab '\t' and blank space '\b'
+ *
+ *With the loose definition that a word is any sequence of characters
+ *or a newline.
+ 
+ *This is a bare-bones version of the UNIX program "wc"
+ */
+
+#include <stdio.h>
+
+/* count lines, words, and characters in input */
+
+int main()
+{
+	int 
+}
