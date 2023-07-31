5d0e7b89731ec019adb81802b71d0a6b3c1b4089
sams-ddev <sams.alxstudent@gmail.com>
2023-07-31 10:48:36 +0000

    wrote the first singly_linked node

---
+++ C_programming-concepts/Linked_lists/0x01-single_list.c
@@ -0,0 +1,25 @@
+#include <stdio.h>	//standard input/output header
+#include <stdlib.h>	//contains the malloc functions
+
+struct node
+{
+	int data;
+	struct node *link;
+};	//Creates a node thats contains data and a link to another node.
+
+int main()
+{
+	struct node *head = NULL;	//creates a header that points to struct node
+	head = (struct node *)malloc(sizeof(struct node));
+	/*uses malloc() to allocates size for the struct called node and saves as a pointer
+	 * to the to the head*/
+
+	head->data = 45;	//head is accessing and writing the data store in the node
+				//the variablae 'data' is given 45
+	
+	head->link = NULL;	//Using the header to access the link adrress
+	
+	
+	printf("%d\n", head->data);
+}
+
