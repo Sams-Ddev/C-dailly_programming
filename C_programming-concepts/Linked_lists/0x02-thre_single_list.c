b89193d3affe25f216772492ff0ef767f323171a
sams-ddev <sams.alxstudent@gmail.com>
2023-07-31 13:46:36 +0000

    creating singly linked method 2

--- C_programming-concepts/Linked_lists/0x02-thre_single_list.c
+++ C_programming-concepts/Linked_lists/0x02-thre_single_list.c
@@ -1,26 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+ * This is the first vavriable of the code
+ */
+
 struct node1
 {
 	int data;
 	struct node *link;
-};
+ii
 
 
 int main()
 {
+	/* Creating the first node */
 	struct node *head = NULL;
 	struct node *head = malloc(sizeof(struct node));
-	
 	head-> data;
 	head-> link;
 
 
+	/* Adding the second node */
 	struct node *secHead = malloc(sizeof(struct node));
 	secHead-> 98;
 	secHead-> NULL;
 	head->link = secHead;
+
+
+	/* Adding the 3rd node */
+	struct node *thrdHead = malloc(sizeof(struct node));
+	thrdHead-> 205;
+	thrdHead-> NULL;
+	sechead->link = thrdHead;
+
 
 
 	return (0);
---
+++ C_programming-concepts/Linked_lists/0x03-thre_single_list2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node1
+{
+		int data;
+		struct node *link;
+};
+
+
+int main()
+{
+		struct node *head = NULL;
+		struct node *head = malloc(sizeof(struct node));
+			
+		head-> data;
+		head-> link;
+
+
+		struct node *secHead = malloc(sizeof(struct node));
+		secHead-> 98;
+		secHead-> NULL;
+		head->link = secHead;
+
+
+		return (0);
+}
+
