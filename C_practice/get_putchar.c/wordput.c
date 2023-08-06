1a9c9b04546a98c599646c3e59e0f72ed5f37efb
sams-ddev <sams.alxstudent@gmail.com>
2023-08-06 21:13:16 +0000

    program not counting the space and tab terminators

--- C_practice/get_putchar.c/wordput.c
+++ C_practice/get_putchar.c/wordput.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
 
 /* This program prints its inputs one per line */
-
+int main(void)
+{
+    int c;
+    
+    while ((c = getchar()) != EOF)
+    {
+        ++c;
+        putchar(c);
+        if (c == ' ' && c == '\n' && c == '\t')
+        {
+            putchar('\n');
+        }
+    }
+    return (0);
+}
