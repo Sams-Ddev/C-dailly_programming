cacfa63687250899fa101f9c7b133c5b9cbae109
sams-ddev <sams.alxstudent@gmail.com>
2023-08-06 12:01:01 +0000

    compiled 0x02-wordCount

--- C_practice/char_count/0x02-wordCount.c
+++ C_practice/char_count/0x02-wordCount.c
@@ -20,8 +20,18 @@ int main()
 	state = OUT;
 	nl = nw = nc = 0;
 
-	while ((c = getchar() != EOF)
+	while ((c = getchar()) != EOF)
+	{
 		++nc;
 		if (c == '\n')
-
+			++nl;
+		if (c == ' ' || c == '\n' || c == '\t')
+			state = OUT;
+		else if (state == OUT)
+		{
+			state = IN;
+			++nw;
+		}
+	}
+	printf("%d %d %d\n", nl, nw, nc);
 }
