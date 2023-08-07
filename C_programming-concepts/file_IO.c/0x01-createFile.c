f0a91a56b8cedb2c6501110aae2fa7f5d97b403e
sams-ddev <sams.alxstudent@gmail.com>
2023-08-07 16:22:53 +0000

    concepts folder on file handling

---
+++ C_programming-concepts/file_IO.c/0x01-createFile.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* This is a demonstration of file handling in C*/
+
+int main(void)
+{
+	/*Declaring a file pointer varaiables*/
+	FILE *fp;
+	fp = fopen("")
+}
