a9200c8bdf17c890aa44cbfac30d0c4a564a7b98
sams-ddev <sams.alxstudent@gmail.com>
2023-08-11 07:36:08 +0000

    rework for 0x02-funCall()

--- C_practice/4-c_functions/0x02-funCall.c
+++ C_practice/4-c_functions/0x02-funCall.c
@@ -24,7 +24,7 @@ int main(void)
 	int x = 10, y = 22;
 
 	sum = funCall(x, y);
-	printf("funCall(x, y) is = %d\n", funCall(x, y));
+	printf("funCall(10, 22) is = %d\n", funCall(x, y));
 
 	return (EXIT_SUCCESS);
 }
