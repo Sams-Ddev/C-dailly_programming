2f7cc795501b00950177ad44aeed9ae544f4b6ce
sams-ddev <sams.alxstudent@gmail.com>
2023-08-11 15:15:32 +0000

    compiled_SUCCESS  for pid.c

--- C_programming-concepts/PID/pid.c
+++ C_programming-concepts/PID/pid.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /**
  * program prints the process ID of a process
@@ -10,6 +11,6 @@ int main(void)
 	pid_t my_pid;
 	my_pid = getpid();
 
-	printf("My process ID is _%u_", my_pid);
-	return ("EXIT_SUCCESS");
+	printf("My process ID is _%u_\n", my_pid);
+	return (EXIT_SUCCESS);
 }
