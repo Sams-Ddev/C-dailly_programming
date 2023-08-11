9dea4fa4558483503409612d4784b16c349391c6
sams-ddev <sams.alxstudent@gmail.com>
2023-08-11 15:46:54 +0000

    wrote ppid.c

---
+++ C_programming-concepts/PID/ppid.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * ppid - get the PID of the parent process
+ * that the same anytime the code i srun
+ */
+
+int main(void)
+{
+	pit_t my_ppid;
+	my_ppid = getppid();
+
+	printf("The parent PID is _%u_", my_ppid);
+	return (EXIT_SUCCESS);
+}
