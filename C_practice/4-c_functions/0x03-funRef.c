1cf8a8b9f17fcac83bc760dcfb6e9fd4648f5a89
sams-ddev <sams.alxstudent@gmail.com>
2023-08-11 07:50:44 +0000

    wrote the funRef()

---
+++ C_practice/4-c_functions/0x03-funRef.c
@@ -0,0 +1,23 @@
+/**
+ * funRef() - is a reference funtion by addresses called
+ * int the main()
+ * @x: parameter one
+ * @y: parameter two
+ *
+ * Return: 0 (EXIT_SUCCESS)
+ */
+
+int funRef(int x, int y)
+{
+	return ();
+}
+
+/**
+ * main - the main calls funRef() by memory address
+ * @funref: function reference in main
+ */
+
+int main(void)
+{
+	int x =100, y = 200;
+}
