66bb69647541a277142ad2ebf1438e175ba3b79f
sams-ddev <sams.alxstudent@gmail.com>
2023-08-11 08:27:41 +0000

    wrote the funShift()

---
+++ C_practice/4-c_functions/GATE2014.c
@@ -0,0 +1,22 @@
+/**
+ * the value of fun(435) is ____
+ */
+
+int funShift(int num)
+{
+	int count = 0;
+	while (num)
+	{
+		count++;
+		num >>= 1;
+	}
+	return (count);
+}
+
+int main(void)
+{
+	funShift(435);
+	printf("The value of funShitf(435) is _%d_", funShift(435));
+
+	return (EXIT_SUCCESS);
+}
