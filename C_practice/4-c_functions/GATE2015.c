3b66564dd4ce81a3e5515bb8426040c69a72bd25
sams-ddev <sams.alxstudent@gmail.com>
2023-08-11 08:52:03 +0000

    wrote the GATE2015.c

---
+++ C_practice/4-c_functions/GATE2015.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * funShift() - does a >> operations
+ * @num: number to operate on.
+ * @count: number of shift >> done
+ * for num = 0.
+ */
+
+int funShift(int num)
+{
+	int count = 2;
+	while (num)
+	{
+		count++;
+		num >>= 2;
+	}
+	return (count);
+}
+
+/**
+ * main - calls funShift
+ */
+
+int main()
+{
+	funShift(435);
+	return (EXIT_SUCCESS);
+}
