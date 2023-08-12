97c9a0860bf5287b3e2d74cac02ec34a47eaa938
sams-ddev <sams.alxstudent@gmail.com>
2023-08-12 07:47:18 +0000

    wrote getline.c

--- C_practice/get_putchar.c/getline.c
+++ C_practice/get_putchar.c/getline.c
@@ -1,6 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
  * A program that prints "$ ", wait for the user to enter a command,
  * prints it on next line.
- * Using "getline"
- *
- *
+ * Using "getline()"
+ */
+
+int read_and_print_line(void)
+{
+	char buffer[1024];
+	char *line;
+	int n;
+
+	printf("$ ");
+	if ((n = getline(&line, &buffer, stdin)) != -1)
+	{
+		printf("%s", line);
+		free(line);
+	}
+	else
+	{
+		perror("Command can not be found");
+	}
+	return (EXIT_SUCCESS);
+}
