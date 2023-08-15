acfe9d28ea9e2e03b24c9f15f03db777eee57d59
sams-ddev <sams.alxstudent@gmail.com>
2023-08-15 21:28:10 +0000

    betty *.c

--- C_programming-concepts/pointers/max_min-pointer.c
+++ C_programming-concepts/pointers/max_min-pointer.c
@@ -1,2 +1,24 @@
-#include <stdio.h
->
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * the simple program finds the lagest and smalllest of an
+ * array of numbers.
+ * @
+ * @
+ * @
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	/* Array elements */
+	int arr[] = {20, 1, 0, 21, 11, 92, 10.5, 201.5};
+	int i, max, min, arr_length;
+
+	int arr_length = sizeof(arr) / sizeof(arr[0]);
+	i = max = min = 0;
+	for ()
+	return (EXIT_SUCCESS);
+}
