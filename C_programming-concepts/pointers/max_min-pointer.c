8d8d8bac87b4a19a09a1248723451ba883460d42
sams-ddev <sams.alxstudent@gmail.com>
2023-08-15 21:41:55 +0000

    testing the arr_len and for loop

--- C_programming-concepts/pointers/max_min-pointer.c
+++ C_programming-concepts/pointers/max_min-pointer.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 
 /**
- * the simple program finds the lagest and smalllest of an
+ * main - the simple program finds the lagest and smalllest of an
  * array of numbers.
- * @
- * @
- * @
+ * @arr: array name
+ * @max: largest number
+ * @min: smallest number
+ * @arr_len: array length
  *
  * Return: Always 0.
  */
@@ -15,10 +16,12 @@ int main(void)
 {
 	/* Array elements */
 	int arr[] = {20, 1, 0, 21, 11, 92, 10.5, 201.5};
-	int i, max, min, arr_length;
+	int i, max, min, arr_len;
 
-	int arr_length = sizeof(arr) / sizeof(arr[0]);
-	i = max = min = 0;
-	for ()
+	arr_len = sizeof(arr) / sizeof(arr[0]);
+	arr[0] = max = min;
+
+	for (i = 0; i < arr_len; i++)
+	printf("arr_len is %d\n", arr_len);
 	return (EXIT_SUCCESS);
 }
